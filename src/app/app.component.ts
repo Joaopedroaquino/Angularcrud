0bddbc1fb4482d209e35643fd27fc903f797dae0
Joaopedroaquino <aqerhack@gmail.com>
2022-03-03 17:20:49 -0300

    refatorando o codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,19 @@
 import { Component } from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Angularproject';
-  constructor(private dialog : MatDialog){
-
-  }
+  constructor(private dialog: MatDialog) {}
 
   openDialog() {
     this.dialog.open(DialogComponent, {
-      width: '30%'
-    
+      width: '30%',
     });
-}
+  }
 }
